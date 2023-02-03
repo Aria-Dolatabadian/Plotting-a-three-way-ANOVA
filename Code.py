ad2f810a21e459f9d84bdf12f7fd90b80d287f11
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-03 13:30:37 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,7 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv (r'Fertilizer2.csv')
+df = pd.read_csv (r'Fertilizer.csv')
+#df = pd.read_csv (r'Fertilizer2.csv')
+
 print(df)
 
 sns.set_theme(style="whitegrid")
