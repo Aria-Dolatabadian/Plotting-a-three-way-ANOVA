2852b60bce8c0b41532fe75b3baa247bd60ed492
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-03 13:27:31 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv (r'Fertilizer2.csv')
+print(df)
+
+sns.set_theme(style="whitegrid")
+
+g = sns.catplot(x="time", y="Plant height", hue="Cultivar", col="Fertilizer",
+                capsize=.2, palette="YlGnBu_d", height=6, aspect=.75,
+                kind="point", data=df)
+g.despine(left=False)
+plt.show()
